3685b0ec1572db356a88555d3c728f09b5ac8e9b
Carlos Eduardo <eduardogomes114@yahoo.com>
2023-02-19 19:53:01 -0300

    teste no index

--- src/Card.js
+++ src/Card.js
@@ -3,12 +3,14 @@ import './Card.css'
 
 const Card = ({titulo, texto}) =>{
     return(
-        <div className="card">
-            <img src="" />
-            <h3>{titulo}</h3>
-            <p>{texto}</p>
-        </div>
+
+            <div className="card">
+                <img src="" />
+                <h3>{titulo}</h3>
+                <p>{texto}</p>
+            </div>
+        
     )
 }
 
-export default Card
+export default Card 
