fbac1a22e2c85913936af91d6de104e4842a8bb2
Carlos Eduardo <eduardogomes114@yahoo.com>
2023-05-15 07:32:31 -0300

    criando rotas

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './Header.css'
 
-function Header({titulo}){
+function Header({titulo, subtitulo}){
     return (
         <header className="header">
             <h1>{titulo}</h1>
+            <h2>{subtitulo}</h2>
         </header>
     )
 }
